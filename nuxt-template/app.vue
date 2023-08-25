bf84c0e54faf055f71c4f53fbac8fc4c6e9bda3f
José Barros <61697278+zzzbarros@users.noreply.github.com>
2023-08-25 19:33:41 -0300

    feat(setup): started project

---
+++ nuxt-template/app.vue
@@ -0,0 +1,12 @@
+<template>
+  <NuxtPage />
+</template>
+
+<script setup lang="ts">
+  useHead({
+    title: 'Nuxt Drink Challenge',
+    meta: [
+      { name: 'description', content: 'Desafio front-end com nuxt - By Faster' }
+    ]
+  })
+</script>
