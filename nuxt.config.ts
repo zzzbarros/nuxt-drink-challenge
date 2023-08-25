9fb55bdfc08ad08c584407865b9541ebc2d76dc3
José Barros <61697278+zzzbarros@users.noreply.github.com>
2023-08-25 19:55:06 -0300

    feat(setup): initialized repository

---
+++ nuxt.config.ts
@@ -0,0 +1,22 @@
+export default defineNuxtConfig({
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Public+Sans:wght@100;200;300;400;500;600;700;800;900&display=swap',
+        },
+      ],
+    },
+  },
+  css: ['@/assets/scss/global.scss'],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "@/assets/scss/_vars.scss" as *;',
+        },
+      },
+    },
+  },
+})
--- src/nuxt.config.ts
+++
@@ -1,3 +0,0 @@
-export default defineNuxtConfig({
-  css: ['@/assets/scss/global.scss'],
-})
