a4232de316cc9ccaea62f4e858acc177bbec074f
José Barros <61697278+zzzbarros@users.noreply.github.com>
2023-08-26 12:25:55 -0300

    chore: remove unused import to mobile category filter

--- components/drink/categoryFilterMobile.vue
+++ components/drink/categoryFilterMobile.vue
@@ -10,7 +10,6 @@
 </template>
 
 <script lang="ts" setup>
-  import {} from '../buttons/tagButton.vue' 
   import type { CategoriesProps } from '@/models/drinks.models'
   
   const emits = defineEmits(['toggleCategory'])
