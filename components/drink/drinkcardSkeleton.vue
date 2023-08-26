543c588de741c803453d85550d5610d6c7e6d4c9
José Barros <61697278+zzzbarros@users.noreply.github.com>
2023-08-26 11:47:04 -0300

    feat(list): added responsiveness to drink card skeleton

--- components/drink/drinkcardSkeleton.vue
+++ components/drink/drinkcardSkeleton.vue
@@ -6,48 +6,41 @@
 </template>
 
 <style lang="scss" scoped>
-.card-skeleton{
-  padding: $xxs;
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  .skeleton {
+  .card-skeleton{
+    padding: $xxs;
     width: 100%;
-    background-color: $gray;
-    animation: pulse 1.5s infinite;
+    height:100%;
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+    .skeleton {
+      width: 100%;
+      background-color: $gray;
+      animation: pulse 1.5s infinite;
+    }
+    .img {
+      border-radius: $xxxs $xxxs 0 0;
+      margin-bottom: 14px;
+      min-height: 144px;
+      @include small {
+        min-height: 238px;
+      }
+      @include medium {
+        min-height: 312px;
+      }
+      @include large {
+        min-height: 308px;
+      }
+      @include xlarge {
+        min-height: 362px;
+      }
+      @include xxlarge {
+        min-height: 272px;
+      }
+    }
+    .p{
+      height: 24px;
+      border-radius: 0 0 $nano $nano;
+    }
   }
-  .img {
-    min-height: 22vh;
-    min-width: 40vw;
-    border-radius: $xxxs $xxxs 0 0;
-    margin-bottom: 14px;
-
-    @include small {
-      min-height: 22vh;
-      min-width: 43vw;
-    }
-    @include medium {
-      min-height: 60vh;
-      min-width: 45vw;
-    }
-    @include large {
-      min-height: 50vh;
-      min-width: 30vw;
-    }
-    @include xlarge {
-      min-height: 59vh;
-      min-width: 24.5vw;
-    }
-    @include xxlarge {
-      min-height: 37.5vh;
-      min-width: 38vh;
-    }
-  }
-  .p{
-    height: 24px;
-    border-radius: 0 0 $nano $nano;
-  }
-}
-
 </style>
